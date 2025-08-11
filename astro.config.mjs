8ea06af1ae22c48f38ba259f1a835e6f428cd54a
Luis Velasco <luiselsw27@gmail.com>
2025-08-11 19:19:18 +0200

    corrección

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,12 +8,6 @@ export default defineConfig({
     output: 'server', // 👈 esto indica que es SSR
     adapter: node({ mode: 'standalone' }),
     vite: {
-      server: {
-      allowedHosts: ['weeklysocial.es', 'www.weeklysocial.es']
-    },
-      preview: {
-      allowedHosts: ['weeklysocial.es', 'www.weeklysocial.es']
-    },
     plugins: [tailwindcss()],
   },
 });
