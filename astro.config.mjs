57011f46e617fd761264cb5430e0c40a2f2e2c99
Luis Velasco <luiselsw27@gmail.com>
2025-08-11 14:56:03 +0200

    pasandolo a ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node'; 
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server', // 👈 esto indica que es SSR
+    adapter: node({ mode: 'standalone' }),
     vite: {
       server: {
       allowedHosts: ['weeklysocial.es', 'www.weeklysocial.es']
